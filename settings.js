7539a775bfd780a68ba24e0b30d37959ffae6006
singgggg <108551039+singgggg@users.noreply.github.com>
2022-08-02 07:28:54 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -24,23 +24,23 @@ global.language = indonesia  //change indonesia to english if you don't understa
 
 // Other
 // Other
-global.botname = "Alphabot-Mdོ" //namabot kalian
-global.ownername= "ᴹᴿ᭄ ZeeoneOfcོ ×፝֟͜×" //nama kalian
-global.myweb ="https://api.zeeoneofc.xyz" //bebas asal jan hapus
-global.youtube = "https://youtube.com/c/ZeeoneOfc" //bebas asal jan hapus
-global.github = "https://zeeone-ofc.github.io/" //bebas
-global.email = "zeeoneofc@gmail.com" //bebas
+global.botname = "𝓗𝓑𝓣 𝓑𝓞𝓣" //namabot kalian
+global.ownername= "ᴹᴿ᭄ HBT_BOT ×፝֟͜×" //nama kalian
+global.myweb ="https://google.com/" //bebas asal jan hapus
+global.youtube = "https://youtube.com/" //bebas asal jan hapus
+global.github = "https://github.com/" //bebas
+global.email = "hbtbot@gmail.com" //bebas
 global.region = "Indonesia" //bebas
 global.timezone = 'Asia/Jakarta' //  timezone wib
-global.premium = ['62887435047326'] //premium user
-global.owner = ["622150996855","6285342106390"] //ganti agar fitur owner bisa di gunakan
-global.ownernomer = "62887435047326" // nomor wa kalian
-global.ownernomerr = "+62887435047326" //nmr wa kalian
-global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
-global.donasi = "./image/donasi.jpg" // foto donasi di folder image
+global.premium = ['6283861230413'] //premium user
+global.owner = ["6283861230413","6283861230413"] //ganti agar fitur owner bisa di gunakan
+global.ownernomer = "6283861230413" // nomor wa kalian
+global.ownernomerr = "+6283861230413" //nmr wa kalian
+global.thumbnail = "./image/bothbt.png" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
+global.donasi = "./image/bothbt.png" // foto donasi di folder image
 global.background_welcome="https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
-global.packname = '© Alphabot-Mdོ' //sticker wm ubah
-global.author = 'Di Buat Oleh ZeeoneOfc' //sticker wm ganti nama kalian
+global.packname = '© HBT_BOT' //sticker wm ubah
+global.author = 'Di Buat Oleh HBT_BOT' //sticker wm ganti nama kalian
 global.sessionName = 'session'
 global.typemenu = 'document'
 
@@ -57,17 +57,17 @@ global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutp
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
 global.mess = {
-    sukses: '🤗Done, Oke Desu~',
-    admin: 'Perintah Ini Hanya Bisa Digunakan Oleh Admin !',
-    botAdmin: 'Perintah Ini Hanya Bisa Digunakan Ketika Bot Menjadi Admin !',
-    owner: 'Perintah Ini Hanya Bisa Digunakan Oleh Owner !',
-    group: 'Perintah Ini Hanya Bisa Digunakan Di Group !',
-    private: 'Perintah Ini Hanya Bisa Digunakan Di Private !',
-	bot: 'Fitur Khusus Pengguna Nomor Bot',
-    errtoimg: 'Maaf Saat Ini Belum Support Sticker Gif !',
-    wait: '⏳ Sedang Di Proses',
-	lockCmd: 'Fitur Tidak Diaktifkan Oleh Owner!',
-	example1: 'Selamat Datang @user Di Group @subject Jangan Lupa Baca Rules @desc\n\nNote :\n1. @user (Mention User Join)\n2. @bio (Get Bio User Join)\n3. @tanggal (Date Now)\n4. @desc (Get Description Group)\n5. @subject (Group Name)'
+    sukses: '🤗Done, Oke Desu~ HBT_BOT',
+    admin: 'Perintah Ini Hanya Bisa Digunakan Oleh Admin ! HBT_BOT',
+    botAdmin: 'Perintah Ini Hanya Bisa Digunakan Ketika Bot Menjadi Admin ! HBT_BOT',
+    owner: 'Perintah Ini Hanya Bisa Digunakan Oleh Owner ! HBT_BOT',
+    group: 'Perintah Ini Hanya Bisa Digunakan Di Group ! HBT_BOT',
+    private: 'Perintah Ini Hanya Bisa Digunakan Di Private ! HBT_BOT',
+	bot: 'Fitur Khusus Pengguna Nomor Bot HBT_BOT',
+    errtoimg: 'Maaf Saat Ini Belum Support Sticker Gif ! HBT_BOT',
+    wait: '⏳ Sedang Di Proses HBT_BOT',
+	lockCmd: 'Fitur Tidak Diaktifkan Oleh Owner! HBT_BOT',
+	example1: 'Selamat Datang @user Di Group @subject Jangan Lupa Baca Rules @desc\n\nNote :\n1. @user (Mention User Join)\n2. @bio (Get Bio User Join)\n3. @tanggal (Date Now)\n4. @desc (Get Description Group)\n5. @subject (Group Name) HBT_BOT'
 }
 
 let file = require.resolve(__filename)
